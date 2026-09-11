49f68613227ab91279b7411d46e6d7ddf35cec5e
RustyStaircase51 <ttish@lakelandcollege.edu>
2026-09-11 21:02:38 +0000

    javascript renders

--- js/homework.js
+++ js/homework.js
@@ -1,10 +1,9 @@
 /*App State*/
 const dashboardState = {
-  name: ["complete homework"],
-  category: ["notes"],
-  status: ["incomplete"],
-  selectedCategory: "all",
-  
+  tasks: [],
+  category: [],
+  status: [],
+  selectedCategory: "all",  
 };
 
 /*Render Functions*/
@@ -13,32 +12,77 @@ function renderTasks() {
 
   taskList.innerHTML = "";
 
-  tasks.forEach(function(task) {
+  if (dashboardState.tasks.length === 0) {
+    taskList.innerHTML = "<p>No tasks yet. Add one to get started.</p>";
+    return;
+  }
+
+  dashboardState.tasks.forEach(function(task) {
     const li = document.createElement("li");
-    li.textContent = task.text;
+    li.textContent = task.title;
     taskList.appendChild(li);
   });
-
 };
+
+
+function CounterFinder() {
+  let incompleteTasksNum = 0
+
+  dashboardState.status.forEach(function(currentStatus){
+    if (currentStatus === "incomplete")
+      incompleteTasksNum += 1;
+    
+  });
+};
+
 
 function renderCounter() {
+  const taskCounter = document.querySelector("#task_counter");
 
+  taskCounter.innerHTML = "";
+
+  if (dashboardState.tasks.length === 0) {
+    taskCounter.innerHTML = "<h2>No tasks to complete.</h2>";
+    return;
+  }
+
+  else {
+    taskCounter.innerHTML = "<h2>"
+
+  }};
 };
 
-function renderEmptyMessage() {
 
-};
 
 
 function render() {
   renderTasks();
-
+  renderCounter();
 };
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*State Functions*/
-function addTask() {
+/*function addTask() {
 
 };
 
@@ -50,15 +94,16 @@ function toggleTask() {
 
 };
 
-function CounterFinder() {
 
-};
-
+*/
 /*Event Functions*/
-function handleTaskSubmit(event) {
+/*function handleTaskSubmit(event) {
 
 };
 
 function handleTaskDelete(event) {
 
 };
+
+render();
+*/
