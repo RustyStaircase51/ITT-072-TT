27b016f062a8b1930587821c270463c6c7b346ba
RustyStaircase51 <ttish@lakelandcollege.edu>
2026-09-14 17:32:00 +0000

    Added Delete Task

--- js/homework.js
+++ js/homework.js
@@ -14,44 +14,54 @@ const dashboardState = {
 
 /*State Functions*/
 
-
-
 function addTask(task, cat) {
-  const duplicateTask = dashboardState.tasks.some(function(task) {
-    return task.title.toLowerCase() === title.toLowerCase();
-  });
+    dashboardState.id.push(Date.now()),
+    dashboardState.tasks.push(task),
+    dashboardState.category.push(cat),
+    dashboardState.status.push(false)
 
-  if (duplicateTask) {
-    showMessage("That task already exists.");
-    return;
-  }
 
-  dashboardState.tasks.push({
-    id: Date.now(),
-    tasks: task,
-    category: cat,
-    status: false
-  });
+  console.log(dashboardState);
 };
 
 
 
 function clearTasks() {
-  let id_array = dashboardState.id   
-  let tasks_array = dashboardState.tasks
-  let category_array = dashboardState.category
-  let status_array = dashboardState.status
+  dashboardState.id = [];
+  dashboardState.tasks = [];
+  dashboardState.category = [];
+  dashboardState.status = [];
+
+
+
+
+  console.log(dashboardState);
+};
+
+function deleteTask(task_name) {
+	let index_number = dashboardState.tasks.indexOf(task_name);
+  let splice_element1 = dashboardState.id.splice(index_number, 1);
+  let splice_element2 = dashboardState.tasks.splice(index_number, 1);
+  let splice_element3 = dashboardState.category.splice(index_number, 1);
+  let splice_element4 = dashboardState.status.splice(index_number, 1);
+
   
-  id_array = [];
-  tasks_array = [];
-  category_array = [];
-  status_array = [];
+  
+  
+  console.log(index_number);
+  console.log(splice_element1);
+  console.log(splice_element2);
+  console.log(splice_element3);
+  console.log(splice_element4);
+  console.log(dashboardState);
 };
 
 
 
 function completeTask(task_name) {
 	task_name.status = true;
+
+  console.log(dashboardState);
 };
 
 
@@ -63,8 +73,9 @@ function CounterFinder() {
     if (currentStatus === "incomplete")
       incompleteTasksNum += 1;
   
-    return incompleteTasksNum;
   });
+  console.log(incompleteTasksNum);
+  return incompleteTasksNum;
 };
 
 
@@ -85,26 +96,36 @@ function renderTasks() {
 
   dashboardState.tasks.forEach(function(task) {
     const li = document.createElement("li");
-    li.textContent = task.title;
+    li.textContent = task;
 
+    
     const com_btn = document.createElement("button");
-    btn.classList.add('complete_button');
-    btn.textContent = "Complete";
+    com_btn.classList.add('complete_button');
+    com_btn.textContent = "Complete";
     com_btn.addEventListener("click", () => {
 		  handleTaskComplete(task);
+      element.remove("complete_button");
       });
 
+
+      
+    const del_btn = document.createElement("button");
+    del_btn.classList.add('delete_button');
+    com_btn.textContent = "Delete";
+    com_btn.addEventListener("click", () => {
+		  handleTaskComplete(task);
+      });  
+
+    
     taskList.appendChild(li);
     taskList.appendChild(com_btn);
-    taskList.appendChild(del_btn);
   });
 };
 
 
-
 function renderCounter() {
   const taskCounter = document.querySelector("#task_counter");
-
+  const incompleteTasksNum = CounterFinder();
   taskCounter.innerHTML = "";
 
   if (dashboardState.tasks.length === 0) {
@@ -113,6 +134,7 @@ function renderCounter() {
   }
 
   else {
+
     taskCounter.innerHTML = "<h2>" + incompleteTasksNum + " tasks remaining</h2>"
 
   }
@@ -131,24 +153,55 @@ function render() {
 
 
 /*Event Functions*/
-function handleTaskSubmit(event) {
-  // gets form input
-  // validates input
-  // calls addTask()
+function handleTaskSubmit() {
+  const form = document.querySelector("#createTask");
+    form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    const task_grabber = document.querySelector("#taskAdd");
+    const category_grabber = document.querySelector("#categoryAdd");
+    
+    const task = task_grabber.value.trim();
+    const category = category_grabber.value;
 
+    if (task === "") {
+      showMessage("That task needs a name.")
+      return;
+    }
 
-  addTask();
+    const duplicateTask = dashboardState.tasks.some(function(current_task) {
+      return current_task.toLowerCase() === task.toLowerCase();
+    });
+
+    if (duplicateTask) {
+      showMessage("That task already exists.");
+      return;
+    }
+    
+    addTask(task, category);
+  });
 };
 
-function handleClearTasks() {
-      com_btn.addEventListener("click", () => {
-		  handleTaskComplete(task);
-      });
-  clearTasks();
-  render();
+
+
+
+function handleClearTasks() {  
+  const clr_btn = document.querySelector("#clear_button");
+  clr_btn.addEventListener("click", () => {
+		  clearTasks();
+      render();
+    });
 };
 
 function handleTaskComplete(event) {
+  
   completeTask(event);
   render();
 };
+
+
+
+
+handleTaskSubmit();
+handleClearTasks();
+render();
