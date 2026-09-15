7ce08e873e98a4af02a95a714452512b3c69acb5
RustyStaircase51 <ttish@lakelandcollege.edu>
2026-09-15 02:21:16 +0000

    CSS + notes

--- js/homework.js
+++ js/homework.js
@@ -14,64 +14,42 @@ const dashboardState = {
 
 /*State Functions: changes data*/
 
-//Works
+//Adds the task information to the corresponding array in dashboardState
 function addTask(task, cat) {
     dashboardState.id.push(Date.now()),
     dashboardState.tasks.push(task),
     dashboardState.category.push(cat),
     dashboardState.status.push(false)
-
-
-  console.log(dashboardState);
 };
 
 
-//Works
+//Emptys all the information from the arrays in dashboardState
 function clearTasks() {
   dashboardState.id = [];
   dashboardState.tasks = [];
   dashboardState.category = [];
   dashboardState.status = [];
-
-
-
-
-  console.log(dashboardState);
 };
 
-//Works
+//Gives the task's name, finds it's index, then splices (removes) all of the task's information from the arrays
 function deleteTask(task_name) {
 	let index_number = dashboardState.tasks.indexOf(task_name);
   let splice_element1 = dashboardState.id.splice(index_number, 1);
   let splice_element2 = dashboardState.tasks.splice(index_number, 1);
   let splice_element3 = dashboardState.category.splice(index_number, 1);
   let splice_element4 = dashboardState.status.splice(index_number, 1);
-
-  
-  
-  
-  console.log(index_number);
-  console.log(splice_element1);
-  console.log(splice_element2);
-  console.log(splice_element3);
-  console.log(splice_element4);
-  console.log(dashboardState);
 };
 
 
-
+//Marks task as complete by replacing the false with true via splicing
 function markTask(task_name) {
 	let index_num = dashboardState.tasks.indexOf(task_name);
   const splice_change_1 = dashboardState.status.splice(index_num, 1, true)
-  
-  console.log(splice_change_1)
-  console.log(index_num)
-  console.log(dashboardState);
 };
 
 
 
-//Works
+//counts all of the incomplete tasks and returns the number of tasks remaining
 function CounterFinder() {
   let incompleteTasksNum = 0
 
@@ -80,11 +58,10 @@ function CounterFinder() {
       incompleteTasksNum += 1;
   
   });
-  console.log(incompleteTasksNum);
   return incompleteTasksNum;
 };
 
-
+//determines the category's name it receives then changes dashboardState's selectedCategory 
 function CategoryChanger(category_name){
   if (category_name === "all"){
     dashboardState.selectedCategory = "all"
@@ -110,20 +87,21 @@ function CategoryChanger(category_name){
 
 
 /*Render Functions: update page*/
+
+
 function renderTasks() {
   const taskList = document.querySelector("#task_list");
 
   taskList.innerHTML = "";
-
+//Tells the user what to do if no tasks are created
   if (dashboardState.tasks.length === 0) {
     taskList.innerHTML = "<p>No tasks yet. Add one to get started.</p>";
     return;
   }
 
-handleCategory();
-    dashboardState.tasks.forEach(function(task) {
-      let indexNumber = dashboardState.tasks.indexOf(task);
+    dashboardState.tasks.forEach(function(task, indexNumber) {
       let taskCategory = dashboardState.category[indexNumber];
+//this will only pick up the task and display it if 1. the selectedCategory is all or 2. the selectedCategory matches the task's category
       if (dashboardState.selectedCategory === "all" || dashboardState.selectedCategory === taskCategory) {
         
 
@@ -132,6 +110,9 @@ handleCategory();
         
       if (dashboardState.status[indexNumber] === false){
           li.style.color = "red";
+      }
+      else if (dashboardState.status[indexNumber] === true){
+          li.style.color = "green";
       }
 
         const com_btn = document.createElement("button");
@@ -159,7 +140,7 @@ handleCategory();
 };
 
 
-//Works
+//shows "no tasks to complete" if they have deleted all tasks or have created no tasks. If there is a task it will tell them how many still need completed
 function renderCounter() {
   const taskCounter = document.querySelector("#task_counter");
   const incompleteTasksNum = CounterFinder();
@@ -178,7 +159,7 @@ function renderCounter() {
 };
 
 
-//Works
+//Calls both render functions
 function render() {
   renderTasks();
   renderCounter();
@@ -190,7 +171,8 @@ function render() {
 
 
 /*Event Functions: User Action*/
-//Works
+
+//gathers information from the form and figures out if it can add it or if it needs to display an error message
 function handleTaskSubmit() {
   const form = document.querySelector("#taskForm");
     form.addEventListener("submit", (event) => {
@@ -201,7 +183,7 @@ function handleTaskSubmit() {
     
     const task = task_grabber.value.trim();
     const category = category_grabber.value;
-
+//Error Validation
     if (task === "") {
       const error_mes = document.querySelector("#error_message") 
       error_mes.innerHTML = ("That task needs a name.");
@@ -211,10 +193,10 @@ function handleTaskSubmit() {
     const duplicateTask = dashboardState.tasks.some(function(current_task) {
       return current_task.toLowerCase() === task.toLowerCase();
     });
-
+//Error Validation
     if (duplicateTask) {
       const error_mes = document.querySelector("#error_message") 
-      error_mes.innerHTML = ("That task already exists.");
+      error_mes.innerHTML = ("That task name already exists.");
       return;
     }
     
@@ -226,7 +208,7 @@ function handleTaskSubmit() {
   });
 };
 
-//Works
+//Gives the Clear All button an event listener
 function handleClearTasks() {  
   const clr_btn = document.querySelector("#clear_button");
   clr_btn.addEventListener("click", () => {
@@ -235,25 +217,25 @@ function handleClearTasks() {
     });
 };
 
-//Works
+
 function handleTaskMark(event) {
   
   markTask(event);
   render();
 };
 
-//Works
+
 function handleTaskDelete(event) {  
 		deleteTask(event);
     render();
 };
 
-
+//handles the dropdown for category change
 function handleCategory() {
   const category_button = document.querySelector("#category");
-  const new_category = category_button.value
+ 
   category_button.addEventListener("click", () => {
-    CategoryChanger(new_category);
+    CategoryChanger(category_button.value);
     render();
   });
 
@@ -263,8 +245,9 @@ function handleCategory() {
 
 
 
-
+//Initalization
 
 handleTaskSubmit();
 handleClearTasks();
+handleCategory();
 render();
