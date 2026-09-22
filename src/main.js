a358aeb1cfd04a860b22cde0845d6c08b2d0a6a9
RustyStaircase51 <ttish@lakelandcollege.edu>
2026-09-22 17:37:22 -0500

    Delete src directory

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import "./style.css";
-import { welcomeMessage } from "./messages.js";
-import { currentYear } from "./year.js";
-
-document.querySelector("#year").textContent = "Fall " + currentYear;
-document.querySelector("#message").textContent = welcomeMessage;
-
-console.log(currentYear);
-console.log(welcomeMessage);
